f065565fb93fe10167699bca54b1fda8e01c6760
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-03-31 11:18:16 -0300

    feat: includes buttons click function

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let yourVoteFor = document.querySelector('.division-1-header span');
+let position = document.querySelector('.division-1-post span');
+let candidate = document.querySelector('.division-1-candidate');
+let warning = document.querySelector('.display_division-2');
+let candidateImages = document.querySelector('.division-1--right');
+let numbers = document.querySelector('.division-1-numbers');
+
+function clicked(n) {
+  alert('clicou em ' + n);
+}
+
+function white() {
+  alert('clicou em branco');
+}
+
+function fix() {
+  alert('clicou em Corrige');
+}
+
+function confirm() {
+  alert('clicou em confirma');
+}
